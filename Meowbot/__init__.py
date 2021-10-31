43c145622f7eecb870c734014c23d2bdb709a759
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-10-31 17:38:36 +0530

    Update __init__.py

--- Meowbot/__init__.py
+++ Meowbot/__init__.py
@@ -11,7 +11,7 @@ from requests import get
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
-from Meowbot.config import Config 
+from .var import Config 
 from var import Var
 StartTime = datetime.datetime.now()
 
