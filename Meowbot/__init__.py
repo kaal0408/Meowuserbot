6e6dac0c739e690fd7f26dc89488cb9abb83224a
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-10-31 17:52:48 +0530

    Update __init__.py

--- Meowbot/__init__.py
+++ Meowbot/__init__.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 from requests import get
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from Meowbot import Config var
+from Meowbot import Config, var
 StartTime = datetime.datetime.now()
 
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
