55aba072c86687019e442d52e99c7d666a986c66
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-31 18:50:32 +0530

    Update vars.py

--- Meowbot/config/vars.py
+++ Meowbot/config/vars.py
@@ -4,10 +4,7 @@ import os
 
 ENV = bool(os.environ.get("ENV", False))
 
-if ENV:
-    pass
+if os.path.exists("config.py"):
+    from config import Development as Config
 else:
-    if os.path.exists("config.py"):
-        pass
-
-# Meowbot
+    from userbot.Config import Config
