7eac3fc1524c1958e870f059e83f82cfc11312fd
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-11-10 21:10:22 +0530

    Update awake.py

--- Meowbot/plugins/awake.py
+++ Meowbot/plugins/awake.py
@@ -20,7 +20,7 @@ mew_pic = Config.ALIVE_PIC or "https://telegra.ph/file/3c2932815330a143fa1a8.png
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 
-  alive_c = f"Meow is online"
+alive_c = f"Meow is online"
 alive_c = "**🔱Meow Meowbot IS Awake🔱 \n\n\n**"
 alive_c += "`My Bot Status \n\n\n`"
 alive_c += f"`Telethon: TELETHON-1.19.0 \n\n`"
